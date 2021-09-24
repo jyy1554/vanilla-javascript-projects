79515c9ec94ca175a16e835f48dd5e7bef0baa6a
jyy1554 <jyy1556@gmail.com>
2021-09-25 02:46:21 +0900

    Improve <, > arrow action and make dots activate

--- index.js
+++ index.js
@@ -1,22 +1,70 @@
-//좌측 화살표 클릭시 이전 이미지 보여주기
-//우측 화살표 클릭시 다음 이미지 보여주기
 const $previous_btn = document.querySelector('.previous');
 const $next_btn = document.querySelector('.next');
 const $image = document.querySelector('.image');
 
+const $dot1 = document.querySelector('.dot1');
+const $dot2 = document.querySelector('.dot2');
+const $dot3 = document.querySelector('.dot3');
+const $dot4 = document.querySelector('.dot4');
+const $dot5 = document.querySelector('.dot5');
+
+let pre_index = 1;
 let index = 1;
 
-function imageSrc(index) {
-  return '/images/image-' + index + '.png';
+//초기 셋팅
+$dot1.style.backgroundColor = '#102738'; //첫번째 점 선택
+
+function changeImage(i) {
+  //이전에 선택된 점 해제
+  document.querySelector('.dot'+pre_index).style.backgroundColor = '#2973a5';
+  index = i;
+  pre_index = index;
+  //새로 선택된 점 색깔 바꾸기
+  document.querySelector('.dot'+index).style.backgroundColor = '#102738';
+  $image.src = '/images/image-' + i + '.png';
 };
 
-//$image.src = "/images/image-5.png";
+//좌측 화살표 클릭시 이전 이미지 보여주기
 $previous_btn.addEventListener('click', function () {
-  --index;
-  $image.src = imageSrc(index);
+  if(index == 1) changeImage(5);
+  else changeImage(--index);
+
+  console.log(index);
 });
 
+//우측 화살표 클릭시 다음 이미지 보여주기
 $next_btn.addEventListener('click', function () {
-  ++index;
-  $image.src = imageSrc(index);
+  if(index == 5) changeImage(1);
+  else changeImage(++index);
+
+  console.log(index);
+});
+
+
+
+
+/* 이미지 하단의 Dot를 누를 경우, 해당 순번의 이미지 보여주기 */
+$dot1.addEventListener('click', function () {
+  changeImage(1);
+  console.log(index);
+});
+
+$dot2.addEventListener('click', function () {
+  changeImage(2);
+  console.log(index);
+});
+
+$dot3.addEventListener('click', function () {
+  changeImage(3);
+  console.log(index);
+});
+
+$dot4.addEventListener('click', function () {
+  changeImage(4);
+  console.log(index);
+});
+
+$dot5.addEventListener('click', function () {
+  changeImage(5);
+  console.log(index);
 });
