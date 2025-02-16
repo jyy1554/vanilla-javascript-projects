8f78a313cb2a70cc7dab9632f9098fdb2ce92169
jyy1554 <jyy1556@gmail.com>
2025-02-16 21:40:57 +0900

    Refactor my solution of 02. Counter

--- 02-counter/1-my-solution/app.js
+++ 02-counter/1-my-solution/app.js
@@ -1,10 +1,39 @@
-let number = document.getElementById('value').innerHTML + 0;
-// .value는 inputs과 같은 form element에만 먹음. innerHTML 써야됨
+// set initial count
+let count = 0;
+// let number = document.getElementById('value').innerHTML + 0; // .value는 inputs과 같은 form element에만 먹음. innerHTML 써야됨
 
-const counter = document.getElementById('value');
-const decreaseBtn = document.querySelector('.decrease');
-const resetBtn = document.querySelector('.reset');
-const increaseBtn = document.querySelector('.increase');
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+// const counter = document.getElementById('value');
+// const decreaseBtn = document.querySelector('.decrease');
+// const resetBtn = document.querySelector('.reset');
+// const increaseBtn = document.querySelector('.increase');
+
+btns.forEach((btn) => {
+    // console.log(btn);
+    btn.addEventListener('click', (e) => {
+        const styles = e.currentTarget.classList;
+
+        if(styles.contains('decrease')) {
+            count--;
+        } else if(styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if(count > 0) {
+            value.style.color = 'green';
+        } else if(count < 0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = '#222';
+        }
+        
+        value.textContent = count;
+    });
+});
 
 decreaseBtn.addEventListener('click', () => {
     number--;
