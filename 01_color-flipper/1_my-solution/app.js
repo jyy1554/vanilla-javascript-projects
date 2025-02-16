083486e511742037cad1f238965fd90a1c1918c8
jyy1554 <jyy1556@gmail.com>
2025-02-16 20:22:18 +0900

    Refactor 01. color flipper

--- 01_color-flipper/1_my-solution/app.js
+++ 01_color-flipper/1_my-solution/app.js
@@ -4,9 +4,15 @@ const buttonElement = document.getElementById('btn');
 const colorElement = document.querySelector('.color'); // getElementsByClassName 사용불가
 
 buttonElement.addEventListener('click', () => {
-    const randomNum = Math.floor(Math.random() * 10 % 4);
-    const color = colors[randomNum];
+    // get random number between 0-3
+    // const randomNum = Math.floor(Math.random() * 10 % 4);
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
-    document.body.style.backgroundColor = color;
-    colorElement.innerText = color; 
+    document.body.style.backgroundColor = colors[randomNumber];
+    colorElement.innerText = colors[randomNumber]; 
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
