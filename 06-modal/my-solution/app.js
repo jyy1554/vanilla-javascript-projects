752db8e8248daf73aef67c0bc47cd6df0d6453c1
jyy1554 <jyy1556@gmail.com>
2025-02-22 10:30:19 +0900

    Refactor my solution of 6.Modal

--- 06-modal/my-solution/app.js
+++ 06-modal/my-solution/app.js
@@ -4,17 +4,17 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalBtn = document.querySelector('.modal-btn');
-const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
 modalBtn.addEventListener('click', () => {
     console.log('modalBtn click');
 
-    modalOverlay.classList.add('open-modal');
+    modal.classList.add('open-modal');
 });
 
 closeBtn.addEventListener('click', () => {
     console.log('closeBtn click');
 
-    modalOverlay.classList.remove('open-modal');
+    modal.classList.remove('open-modal');
 });
