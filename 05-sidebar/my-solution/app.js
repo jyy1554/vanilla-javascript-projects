0cd8649d09a7251aca6c8b0f13ef1a052dfb1cd3
jyy1554 <yeontaek@Youyeons-MacBook-Air.local>
2025-02-22 09:27:05 +0900

    Refactor my solution of 5.Sidebar

--- 05-sidebar/my-solution/app.js
+++ 05-sidebar/my-solution/app.js
@@ -1,10 +1,17 @@
-const sidebarToggle = document.querySelector('.sidebar-toggle');
+const toggleBtn = document.querySelector('.sidebar-toggle');
+// const sidebarToggle = document.querySelector('.sidebar-toggle');
 const sidebar = document.querySelector('.sidebar');
 const closeBtn = document.querySelector('.close-btn');  
 
-sidebarToggle.addEventListener('click', () => {
+toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('show-sidebar');
     console.log('sidebar click');
+
+    // if(sidebar.classList.contains('show-sidebar')) {
+    //     sidebar.classList.remove('show-sidebar');
+    // } else {
+    //     sidebar.classList.add('show-sidebar');
+    // }
 });
 
 closeBtn.addEventListener('click', () => {
