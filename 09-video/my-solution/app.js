b7fd2bc0708107ae0571c91a90af60b323f50f28
jyy1554 <yeontaek@Youyeons-MacBook-Air.local>
2025-02-23 22:35:18 +0900

    Fix my unfinished solution of 9.Video with 'load' event

--- 09-video/my-solution/app.js
+++ 09-video/my-solution/app.js
@@ -3,19 +3,20 @@
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
 const btn = document.querySelector('.switch-btn');
-const preload = document.querySelector('.preloader');
 const video = document.querySelector('.video-container');
-
-window.addEventListener('DOMContentLoaded', () => {
-
-});
+const preloader = document.querySelector('.preloader');
 
 btn.addEventListener('click', () => {
-    if(!video.classList.contains('stop')) {
+    btn.classList.toggle('slide');
+
+    if(btn.classList.contains('slide')) {
         video.pause();
     } else {
         video.play();
     }
+});
 
-    video.classList.toggle('stop');
+// preloader
+window.addEventListener('load', () => { // 웹페이지의 로드가 완료되었을때 (문서의 모든 콘텐츠가 로드된 후 실행). DOMContentLoaded는 DOM 트리가 완성되자마자 실행
+    preloader.remove('hide-preloader');
 });
